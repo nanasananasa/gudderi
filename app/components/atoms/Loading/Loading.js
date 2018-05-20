4fcb74318326df478a3a2c6001c26d31efdf1849
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2018-05-20 19:02:11 +0900

    フルスクリーンモードと部分表示モードで両方対応する

--- app/components/atoms/Loading/Loading.js
+++ app/components/atoms/Loading/Loading.js
@@ -1,14 +1,26 @@
+/**
+ * @flow
+ */
 import React from 'react';
 import { Container, Spinner, Text } from 'native-base';
 import styles from './LoadingStyles';
 import constants from '../../../constants/constants';
 
-function Loading() {
+function Loading(props: {
+  viewFullScreen: boolean,
+}) {
+  const { viewFullScreen } = props;
+
+  if (viewFullScreen) {
+    return (
+      <Container style={styles.container}>
+        <Spinner color={constants.mainColor} />
+        <Text>読み込み中...</Text>
+      </Container>
+    );
+  }
   return (
-    <Container style={styles.container}>
-      <Spinner color={constants.mainColor} />
-      <Text>読み込み中...</Text>
-    </Container>
+    <Spinner color={constants.mainColor} />
   );
 }
 export default Loading;
